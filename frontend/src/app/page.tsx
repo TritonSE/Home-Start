0935e6413496c410bba96226cf2be46dd0c211c9
Edward C Yao <edward.c.yao@gmail.com>
2026-01-19 12:52:36 -0800

    search bar prototype completed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import VolunteerTable from "@/components/VolunteerTable";
+import SearchBar from "@/components/SearchBar";
 import styles from "./page.module.css";
 
 export default function Page() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <SearchBar />
         <VolunteerTable />
       </main>
     </div>
--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -9,11 +9,20 @@ export default function SearchBar() {
   return (
     <div className={styles.searchBar}>
       <div className={styles.searchField}>
-        <span className={styles.inputField}>
+        <div className={styles.inputField}>
+          <span className={styles.ic_search}>
+            <img src="/Union.svg" alt="Union logo" className={styles.union} />
+          </span>
           <form className={styles.textField}>
-            <input type="text" value={search} placeholder="Search Volunteer"></input>
+            <input type="text" value={search} placeholder="Search Volunteer" ></input>
           </form>
-        </span>
+        </div>
+      </div>
+
+      <div className={styles.tagsContainer}>
+        <button className={styles.pillTagAll}> <span className={styles.pillTagText}> All </span> </button>
+        <button className={styles.pillTagType}> <span className={styles.pillTagText}>Volunteer Type</span> </button>
+        <button className={styles.pillTagAdded}> <span className={styles.pillTagText}>Added By</span> </button>
       </div>
     </div>
   );

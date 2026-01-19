c7e1975cd9ca7ef7bc6195f6fffcc5e7bc1f8535
Edward C Yao <edward.c.yao@gmail.com>
2026-01-19 12:52:54 -0800

    search bar prototype completed

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -14,15 +14,24 @@ export default function SearchBar() {
             <img src="/Union.svg" alt="Union logo" className={styles.union} />
           </span>
           <form className={styles.textField}>
-            <input type="text" value={search} placeholder="Search Volunteer" ></input>
+            <input type="text" value={search} placeholder="Search Volunteer"></input>
           </form>
         </div>
       </div>
 
       <div className={styles.tagsContainer}>
-        <button className={styles.pillTagAll}> <span className={styles.pillTagText}> All </span> </button>
-        <button className={styles.pillTagType}> <span className={styles.pillTagText}>Volunteer Type</span> </button>
-        <button className={styles.pillTagAdded}> <span className={styles.pillTagText}>Added By</span> </button>
+        <button className={styles.pillTagAll}>
+          {" "}
+          <span className={styles.pillTagText}> All </span>{" "}
+        </button>
+        <button className={styles.pillTagType}>
+          {" "}
+          <span className={styles.pillTagText}>Volunteer Type</span>{" "}
+        </button>
+        <button className={styles.pillTagAdded}>
+          {" "}
+          <span className={styles.pillTagText}>Added By</span>{" "}
+        </button>
       </div>
     </div>
   );

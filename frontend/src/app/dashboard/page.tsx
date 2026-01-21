0745183db797b59504d2273210b7014774378e5f
冨岡哲平 <tomiokateppei@Yuzumac2.local>
2026-01-21 08:38:53 -0800

    Add desktop sidebar navigation and adjust dashboard layout

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -6,34 +6,33 @@ import importExport from "../../../public/ion_document.png";
 
 export default function Dashboard() {
   return (
-    <div className={styles.outerMostDiv}>
-      {/* Navigation Bar*/}
-      <div className={styles.navigationBar}></div>
-      {/* Dashboard */}
-      <div className={styles.dashboard}>
-        <div className={styles.dashboardInnerFrame}>
-          <div className={styles.dashboardInnerMostFrame}>
-            <p className={styles.headersH2}>Dashboard</p>
-          </div>
+    <div className={styles.dashboard}>
+      <div className={styles.dashboardHeader}>
+        <p className={styles.headersH2}>Dashboard</p>
+      </div>
+
+      <div className={styles.dashboardSecond}>
+        <div className={styles.quickActions}>
+          <p className={styles.headersH4}>Quick Actions</p>
+          <p className={styles.bodyMd}>Select an action to get started</p>
         </div>
-        <div className={styles.dashboardSecond}>
-          <div className={styles.quickActions}>
-            <p className={styles.headersH4}>Quick Actions</p>
-            <p className={styles.bodyMd}>Select an action to get started</p>
-          </div>
-          <div className={styles.cards}>
-            <Card
-              icon={icMessage}
-              majorText="Send Text"
-              minorText="Reach volunteers instantly via SMS"
-            />
-            <Card icon={mail} majorText="Send Email" minorText="Send detailed announcements" />
-            <Card
-              icon={importExport}
-              majorText="Import/Export Data"
-              minorText="Manage volunteer information"
-            />
-          </div>
+
+        <div className={styles.cards}>
+          <Card
+            icon={icMessage}
+            majorText="Send Text"
+            minorText="Reach volunteers instantly via SMS"
+          />
+          <Card
+            icon={mail}
+            majorText="Send Email"
+            minorText="Send detailed announcements"
+          />
+          <Card
+            icon={importExport}
+            majorText="Import/Export Data"
+            minorText="Manage volunteer information"
+          />
         </div>
       </div>
     </div>
